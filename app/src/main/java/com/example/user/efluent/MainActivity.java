6ed143d4902b23b095f5a3e46dbd2e13750a4e8b
martin.volpe@gmail.com <martin.volpe@gmail.com>
2016-05-12 09:36:50 +0200

    Register Layout added

--- app/src/main/java/com/example/user/efluent/MainActivity.java
+++ app/src/main/java/com/example/user/efluent/MainActivity.java
@@ -5,15 +5,16 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.content.Intent;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        EditText text = new EditText(this);
-        Intent intent1 = new Intent(this, )
+        setContentView(R.layout.activity_main);
+        /*EditText text = new EditText(this);
+        //Intent intent1 = new Intent(this, )
         text.setText("Bonjour, c'est bon rentrez chez vous ");
-        setContentView(text);
+        setContentView(text);*/
     }
 }
