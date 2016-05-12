ac6efa5460ae318bc2bd54d657eff4c5cc802962
martin.volpe@gmail.com <martin.volpe@gmail.com>
2016-05-12 09:45:49 +0200

    Added test callback

--- app/src/main/java/com/example/user/efluent/MainActivity.java
+++ app/src/main/java/com/example/user/efluent/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.user.efluent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.content.Intent;
@@ -12,9 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.connexionMain);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Log.i("test", "mesage");
+            }
+        });
+
+
+
         /*EditText text = new EditText(this);
         //Intent intent1 = new Intent(this, )
         text.setText("Bonjour, c'est bon rentrez chez vous ");
         setContentView(text);*/
     }
 }
+
+
