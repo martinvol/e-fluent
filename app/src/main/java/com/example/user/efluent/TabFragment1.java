5dc990bd7cdeb2cf5199c7db06932bc56b6f049a
martin.volpe@gmail.com <martin.volpe@gmail.com>
2016-05-23 17:29:33 +0200

    Added logic to list

--- app/src/main/java/com/example/user/efluent/TabFragment1.java
+++ app/src/main/java/com/example/user/efluent/TabFragment1.java
@@ -2,14 +2,27 @@ package com.example.user.efluent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
-public class TabFragment1 extends Fragment {
+public class TabFragment1 extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.tab_fragment_1, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.rowlayout, R.id.label,values);
+        setListAdapter(adapter);
     }
 }
