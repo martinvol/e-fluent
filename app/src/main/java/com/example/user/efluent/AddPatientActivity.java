758af4677aa81f57e2cf822d4a9e3cc8eb91cc55
martin.volpe@gmail.com <martin.volpe@gmail.com>
2016-06-06 10:27:55 +0200

    Get strings from AppPatientActivity

--- app/src/main/java/com/example/user/efluent/AddPatientActivity.java
+++ app/src/main/java/com/example/user/efluent/AddPatientActivity.java
@@ -1,10 +1,12 @@
 package com.example.user.efluent;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AddPatientActivity extends AppCompatActivity {
 
@@ -17,9 +19,25 @@ public class AddPatientActivity extends AppCompatActivity {
 
         // Callbacks
         final Button button = (Button) findViewById(R.id.CreatePatient);
+        assert button != null;
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
+
+                String last_name = ((EditText)findViewById(R.id.AddPatientName)).toString();
+                String name = ((EditText)findViewById(R.id.AddPatientFirstName)).toString();
+                String password = ((EditText)findViewById(R.id.AddPatientPassword)).toString();
+
+                if (last_name .equals("") || name.equals("") || password.equals("")){
+                    /*new AlertDialog.Builder(context)
+                            .setTitle("Delete entry")
+                            .setMessage("Are you sure you want to delete this entry?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // continue with delete
+                                }
+                            })*/
+                }
 
                 //login.addPatient();
                 Log.i("test", "from the button");
