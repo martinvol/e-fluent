e641cf668ece61e739442a83ee95ebe672794da5
Martin Volpe <martin.volpe@gmail.com>
2016-06-14 02:41:35 -0400

    Updated settings.

--- backend_server/efluent_webserver/efluent_webserver/settings.py
+++ backend_server/efluent_webserver/efluent_webserver/settings.py
@@ -128,12 +128,13 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.permissions.AllowAny',
     )
 }
 
