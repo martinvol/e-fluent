3800a5c1b8b0d711d18afb44fcca35e7653676d9
Farhan Annas <farhan.annas@gmail.com>
2016-06-15 10:06:06 +0200

    GiveRDV fixed

--- app/src/main/java/com/example/user/efluent/FragmentTestHideToolbar.java
+++ app/src/main/java/com/example/user/efluent/FragmentTestHideToolbar.java
@@ -1,7 +1,55 @@
 package com.example.user.efluent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by User on 15/06/2016.
+ * Created by Arif on 15/06/2016.
  */
 public class FragmentTestHideToolbar {
+
+    /* public final static String ITEMS_COUNT_KEY = "PartThreeFragment$ItemsCount";
+
+    public static FragmentTestHideToolbar createInstance(int itemsCount) {
+        FragmentTestHideToolbar partThreeFragment = new FragmentTestHideToolbar();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ITEMS_COUNT_KEY, itemsCount);
+        partThreeFragment.setArguments(bundle);
+        return partThreeFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.fragment_part_three, container, false);
+        setupRecyclerView(recyclerView);
+        return recyclerView;
+    }
+
+    private void setupRecyclerView(RecyclerView recyclerView) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(createItemList());
+        recyclerView.setAdapter(recyclerAdapter);
+    }
+
+    private List<String> createItemList() {
+        List<String> itemList = new ArrayList<>();
+        Bundle bundle = getArguments();
+        if(bundle!=null) {
+            int itemsCount = bundle.getInt(ITEMS_COUNT_KEY);
+            for (int i = 0; i < itemsCount; i++) {
+                itemList.add("Item " + i);
+            }
+        }
+        return itemList;
+    } */
 }
